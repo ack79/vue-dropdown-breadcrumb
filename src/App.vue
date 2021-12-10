ed9ae6ea6a5aac60f42ddbb6fe377df409692d97
devrimayyildi96 <84701045+devrimayyildi96@users.noreply.github.com>
2021-12-11 00:29:28 +0300

    MOBİLİCORPUS - dummy URL generated

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,47 @@
 import BreadCrumb from "./components/BreadCrumb.vue";
 export default {
   name: "App",
+  data () {
+    return {
+      dummyBreadcrumbUrls: [
+        {
+          label: "Ana Sayfa",
+          link: "https://dementor.com",
+          isDropdown: false,
+          list: null,
+        },
+        {
+          label: "Telefon ve Aksesuarlar",
+          link: "https://dementor.com/telefon-aksesuarlar",
+          isDropdown: false,
+          list: null,
+        },
+        {
+          label: "Cep Telefonları",
+          isDropdown: true,
+          list: [
+            {
+              label: "Android Telefonlar",
+              link: "https://dementor.com/android-telefonlar",
+            },
+            {
+              label: "İphone İOS Telefonlar",
+              link: "https://dementor.com/iphone-telefonlar",
+            },
+            {
+              label: "Diğer Telefonlar",
+              link: "https://dementor.com/diger-telefonlar",
+            },
+          ],
+        },
+        {
+          label: "Android Telefonlar",
+          isDropdown: false,
+          list: null,
+        },
+      ],
+    };
+  },
   components: {
     BreadCrumb,
   },
